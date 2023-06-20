25afcb026084181596b6f3b912a24fe2cb295618
aaronworsnop <aaronworsnop@gmail.com>
2023-06-20 17:24:33 +1200

    Text and leetcode API changes

--- leetcode-api.js
+++ leetcode-api.js
@@ -1,25 +1,39 @@
 const leetcodeApi = 'https://leetcode-stats-api.herokuapp.com/Bobwayde';
+const statisticsElement = document.getElementById('leetcode-statistics');
 
-fetch(leetcodeApi)
-    .then(response => response.json())
-    .then(data => {
+// Display "Retrieving data" message
+statisticsElement.innerHTML = '<p>Retrieving data...</p>';
+
+// Function to fetch statistics and handle retries
+const fetchStatistics = async () => {
+    try {
+        const response = await fetch(leetcodeApi);
+        const data = await response.json();
+
         if (data.status === 'success') {
             const totalSolved = data.totalSolved;
             const easySolved = data.easySolved;
             const mediumSolved = data.mediumSolved;
             const hardSolved = data.hardSolved;
-            
-            const statisticsElement = document.getElementById('leetcode-statistics');
+
+            // Update statistics
             statisticsElement.innerHTML = `
-                <p>Total Solved: ${totalSolved}</p>
-                <p>Easy Solved: ${easySolved}</p>
-                <p>Medium Solved: ${mediumSolved}</p>
-                <p>Hard Solved: ${hardSolved}</p>
+                <h4>Total Solved: ${totalSolved}</h4>
+                <h4>Easy Solved: ${easySolved}</h4>
+                <h4>Medium Solved: ${mediumSolved}</h4>
+                <h4>Hard Solved: ${hardSolved}</h4>
             `;
         } else {
             console.error('Failed to retrieve statistics:', data.message);
+            // Retry after a delay (e.g., 3 seconds)
+            setTimeout(fetchStatistics, 3000);
         }
-    })
-    .catch(error => {
+    } catch (error) {
         console.error('Error:', error);
-    });
+        // Retry after a delay (e.g., 3 seconds)
+        setTimeout(fetchStatistics, 3000);
+    }
+};
+
+// Initial fetch
+fetchStatistics();
