0ee96748395b05962df08be2294a3faf9a1d1881
aaronworsnop <aaronworsnop@gmail.com>
2023-06-21 12:58:15 +1200

    Card glow

--- portfolio-script.js
+++ portfolio-script.js
@@ -20,6 +20,21 @@ function doDarkMode(element) {
     }
 }
 
+for(const card of document.querySelectorAll(".card")) {
+    card.onmousemove = e => handleOnMouseMove(e);
+}
+
+const handleOnMouseMove = (e) => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
+
+    target.style.setProperty('--mouse-x', `${ x }px`);
+    target.style.setProperty('--mouse-y', `${ y }px`);
+}
+
 /**
  * Make dark mode work
  * Add more top padding to the grid area
