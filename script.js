40e62738a30c2274bde9fcd49ed03942e672c538
aaronworsnop <aaronworsnop@gmail.com>
2023-07-05 14:57:46 +1200

    Separating mail functionality from main script file

--- script.js
+++ script.js
@@ -380,46 +380,6 @@ function doDarkMode(element) {
     }
 }
 
-// Contact pane
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-
-    // disable submit button
-    var submitButton = this.querySelector('button[type="submit"]');
-    submitButton.disabled = true;
-    submitButton.classList.add('submit-sending');
-
-    // Get form data
-    var formData = new FormData(this);
-
-    // Create AJAX request
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'mail.php', true);
-
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            // Success and clear form
-            alert('Email sent successfully!');
-            document.getElementById('contact-form').reset();
-        } else {
-            var errorMessage = 'An error occurred with the contact pane. Please email me directly instead.';
-            if (xhr.status === 500) {
-                errorMessage = 'Internal server error. Please try again later or email me directly instead.';
-            } else if (xhr.status === 400) {
-                errorMessage = 'Bad request. Please check the contact pane and ensure you entered your details correctly.';
-            }
-            // Display error message to the user
-            alert(errorMessage);
-        }
-        // re-enable submit button
-        submitButton.disabled = false;
-        submitButton.classList.remove('submit-sending');
-    };
-
-    // Send AJAX request with form data
-    xhr.send(formData);
-});
-
 function toggleLinks() {
     const resume = document.querySelector(".link-git");
     const contact = document.querySelector(".link-in");
