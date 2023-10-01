afcbfa6713e462a4c0bed849c225ee24a62a24d7
aaronworsnop <aaronworsnop@gmail.com>
2023-10-01 22:49:09 +1300

    fix: Yesterday's commits

--- github-api.js
+++ github-api.js
@@ -1,106 +1,37 @@
-// Replace with your GitHub username and access token
 const username = 'aaronworsnop';
 const accessToken = 'PLACEHOLDER';
-
-// Define the GitHub API base URL
 const apiUrl = 'https://api.github.com';
 
-// Create headers with the access token for authentication
-const headers = {
-  Authorization: `Bearer ${accessToken}`,
-};
-
-// Function to get the number of commits made yesterday
-async function getCommitsYesterday() {
-    try {
-      // Calculate the date for yesterday
-      const yesterday = new Date();
-      yesterday.setDate(yesterday.getDate() - 1);
-      const yesterdayISO = yesterday.toISOString().split('T')[0];
-  
-      const response = await fetch(`${apiUrl}/search/commits?q=author:${username}+committer-date:${yesterdayISO}&sort=committer-date&order=desc`, {
-        headers,
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const data = await response.json();
-      const commitCount = data.total_count;
-      console.log(`Commits made yesterday: ${commitCount}`);
-    } catch (error) {
-      console.error('Error fetching commit count:', error.message);
-    }
-  }
-
-// Function to get the number of repositories
-async function getRepositoryCount() {
+async function getCommitsOnDate(date) {
   try {
-    const response = await fetch(`${apiUrl}/users/${username}`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    const repoCount = data.public_repos;
-    console.log(`Total repositories: ${repoCount}`);
-  } catch (error) {
-    console.error('Error fetching repository count:', error.message);
-  }
-}
-
-// Function to get the number of pull requests (PRs) made or merged
-async function getPullRequestCount() {
-  try {
-    const response = await fetch(`${apiUrl}/search/issues?q=type:pr+author:${username}+is:merged`, {
-      headers,
+    const response = await fetch(`${apiUrl}/search/commits?q=author:${username}+committer-date:${date}&sort=committer-date&order=desc`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        Accept: 'application/vnd.github.cloak-preview',
+      },
     });
+
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
+
     const data = await response.json();
-    const prCount = data.total_count;
-    console.log(`Total merged PRs: ${prCount}`);
+    console.log(`\nCommits on ${date}: ${data.total_count}`);
+    data.items.forEach(item => {
+      console.log(`- ${item.commit.message} in ${item.repository.full_name}`);
+    });
   } catch (error) {
-    console.error('Error fetching PR count:', error.message);
+    console.error('Error fetching commit count:', error.message);
   }
 }
 
-// Function to get the total number of commits made
-async function getTotalCommitCount() {
-    try {
-    const response = await fetch(`${apiUrl}/users/${username}/repos?per_page=1000`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const repos = await response.json();
-    let totalCommits = 0;
-
-    for (const repo of repos) {
-      const commitsResponse = await fetch(`${apiUrl}/repos/${username}/${repo.name}/commits`, {
-        headers,
-      });
-      if (!commitsResponse.ok) {
-        throw new Error(`HTTP error! Status: ${commitsResponse.status}`);
-      }
-      const commitsData = await commitsResponse.json();
-      totalCommits += commitsData.length;
-    }
-
-    console.log(`Total commits: ${totalCommits}`);
-  } catch (error) {
-    console.error('Error fetching total commit count:', error.message);
-  }
-}
-
-// Function to get the date of yesterday in ISO format (YYYY-MM-DD)
 function getYesterday() {
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 2);
-    return yesterday.toISOString().split('T')[0];
-  }
-  
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  return yesterday.toISOString().split('T')[0];
+}
 
-// Fetch and display the information
-getCommitsYesterday();
-getRepositoryCount();
-getPullRequestCount();
-getTotalCommitCount();
+// Example usage:
+console.log(`Fetching commit data for user: ${username}`);
+getCommitsOnDate(getYesterday());
